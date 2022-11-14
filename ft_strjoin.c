5ea514e28cd54f8be1ebab2fd9bc05e93734bb38
Vinícius Sá Porto <73244946+viniciussaporto@users.noreply.github.com>
2022-11-14 19:38:45 +0000

    corrected in thory due tomorrow

---
+++ ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsa-port <vsa-port@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 21:56:41 by vsa-port          #+#    #+#             */
+/*   Updated: 2022/11/08 18:01:58 by vsa-port         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	res = (char *)malloc(sizeof(char ) * (s1_len + s2_len + 1));
+	if (!res)
+		return (NULL);
+	ft_memcpy(res, s1, s1_len);
+	ft_memcpy(res + s1_len, s2, s2_len);
+	res[s1_len + s2_len] = '\0';
+	return (res);
+}
