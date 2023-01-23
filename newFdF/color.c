7b5bf91e71aa5facb44b42f8d01430c0f99190f0
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-01-23 17:01:07 +0000

    1/4 done

--- newFdF/color.c
+++ newFdF/color.c
@@ -6,7 +6,7 @@
 /*   By: vsa-port <vsa-port@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 14:52:26 by vsa-port          #+#    #+#             */
-/*   Updated: 2023/01/20 15:27:24 by vsa-port         ###   ########.fr       */
+/*   Updated: 2023/01/23 10:47:53 by vsa-port         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,46 @@ int	create_trgb(int t, int r, int g, int b)
 	return (t << 24 | r << 16 | g << 8 | b);
 }
 
-dsa-mora
+int	make_color(float perccent, int flag, int r, int g)
+{
+	if (flag == 0)
+		return (create_trgb(0, 255, 255, 255));
+	else if (flag == 1)
+		return (create_trgb(0, percent * 255, 0, 0));
+	else if (flag == 2)
+		return (create_trgb(0, 0, percent * 255, 0));
+	else if (flag == 3)
+		return (create_trgb(0, 0, 0, percent * 255));
+	else if (flag == 4)
+		return (create_trgb(0, g, r, 0));
+	else if (flag == 5)
+		return (create_trgb(0, 0, r, g));
+	else if (flag == 6)
+		return (create_trgb(0, g, 0, r));
+	else if (flag == 7)
+		return (create_trgb(0, r, g, g));
+	else if (flag == 8)
+		return (create_trgb(0, g, r, g));
+	else if (flag == 9)
+		return (create_trgb(0, g / 3, r / 0.5, r / 1.25));
+	return (0);
+}
+
+int	percent_to_color(float percent, int flag)
+{
+	int	r;
+	int	g;
+
+	r = 255;
+	g = 255;
+	if (flag >= 4 && flag <= 9)
+	{
+		if (percent < 0.5)
+			g = (255 * percent / 0.5);
+		else
+			r = 255 - (255 * (percent - 0.5) / 0.5);
+		return (make_color(percent, flag, r, g));
+	}
+	else
+		return (make_color(percent, flag, 0, 0));
+}
---
+++ newFdF/draw_grid.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_grid.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsa-port <vsa-port@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 10:54:57 by vsa-port          #+#    #+#             */
+/*   Updated: 2023/01/23 15:15:50 by vsa-port         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+t_point	transformations(t_vars *vars, t_point a)
+{
+	a = rotation_x(a, vars);
+	a = rotation_y(a, vars);
+	a = rotation_z(a, vars);
+	ft_max_and_min(a.z, vars);
+	if (vars->tranform_number == 1)
+		a = isometric_projection(a, vars);
+	if (vars->tranform_number == 2)
+		a = parallel_projection(a, vars);
+	if (vars->tranform_number == 3)
+		a = top_view(a, vars);
+	if (vars->tranform_number == 4)
+		a = right_view(a, vars);
+	if (vars->tranform_number == 5)
+		a = front_view(a, vars);
+	return (a);
+}
+
+void	edge_case(t_vars *vars, t_point a, t_point b, float range, float x)
+{
+	int	y;
+	int	flag;
+
+	flag = 0;
+	y = a.y;
+	while (fabs(b.y - y) > 0.5)
+	{
+		vars->rgb_p = vars->rgb_p + range / ((vars->size_grid));
+		my_mlx_pixel_put(vars, x , y, percent_to_color(vars->rgb_p, \
+			vars->flag));
+		y += ((b.y - a.y) / (fabs(b.y - a.y)));
+		flag++;
+	}
+	return ;
+}
+
+void	initializer(t_vars*vars, t_point a, t_point b, float x)
+{
+	vars->rgb_p = a.z / vars->new_max;
+	if (b.z - a.z > 0)
+		vars->range_z = (b.z / vars->new_max - vars->rgb_p);
+	else
+		vars->range_z = -(a.z / vars->new_max - b.z / vars->new_max);
+	if (fabs(b.x - a.x) <= 0.5 && fabs(b.y - a.y) > 0.5)
+		edge_case(vars, a, b, vars->range_z, x);
+	vars->slope = (b.y - a.y) / (b.x - a.x);
+	vars->direction = (b.x - a.x) / (fabs(b.x - a.x));
+}
+
+void	two_points(t_vars *vars, t_point a, t_point b)
+{
+	float	x;
+	float	y;
+	
+	a = transformations(vars, a);
+	b = transformations(vars, b);
+	x = a.x;
+	initializer(vars, a, b, x);
+	while(fabs(b.x -x) > 0.5)
+	{
+		y = vars->slope * (x - a.x) + a.y;
+		vars->rgb_p = vars->rgb_p +vars->range_z / ((vars->size_grid));
+		my_mlx_pixel_put(vars, x, y, percent_to_color(vars->rgb_p, vars->flag));
+		if ((fabs(y - (vars->slope * ((x + vars->direction) - a.x) + a.y))) > 1)
+		{
+			while ((int)(y - (vars->slope * \
+				((x + vars->direction) - a.x) + a.y)))
+			{
+				y += ((b.y - a.y) / (fabs(b.y - a.y)));
+				my_mlx_pixel_put(vars, x, y, \
+				percent_to_color(vars->rgb_p, vars->flag));
+			}
+		}
+		x += vars->direction;
+	}
+}
+
+void	draw_img_grid(t_vars *vars)
+{
+	int	x;
+	int	y;
+	int	y_new;
+	
+	y = -1;
+	while (++y < vars->)
+}
