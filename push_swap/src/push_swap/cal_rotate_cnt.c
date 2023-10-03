8da7d0fd98f25723e12972e40481cdcce60c2b28
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-10-03 18:34:35 +0100

    only checker missing

--- push_swap/src/push_swap/cal_rotate_cnt.c
+++ push_swap/src/push_swap/cal_rotate_cnt.c
@@ -6,7 +6,7 @@
 /*   By: vsa-port <vsa-port@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 14:28:41 by vsa-port          #+#    #+#             */
-/*   Updated: 2023/10/03 14:46:47 by vsa-port         ###   ########.fr       */
+/*   Updated: 2023/10/03 15:18:50 by vsa-port         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	find_location(t_deque *deque_a, t_deque *deque_b, int idx)
 
 int	cal_src_rotate(int idx, t_deque *deque_b)
 {
-	int	rotate;
+	int	range;
 
 	range = deque_b->total_cnt / 2;
 	if (idx <= range)
@@ -54,5 +54,37 @@ int	cal_dst_rotate(t_deque *deque_a, t_deque *deque_b, int idx)
 {
 	int	location;
 
-	location = find_location
+	location = find_location(deque_a, deque_b, idx);
+	if (!location)
+		return (0);
+	else
+	{
+		if (location < deque_a->total_cnt / 2)
+			return (location *= -1);
+		else
+			return (deque_a->total_cnt - location);
+	}
+}
+
+int	cal_total_rotate(int src_rotate, int dst_rotate)
+{
+	if (src_rotate * dst_rotate > 0)
+	{
+		if (src_rotate < 0)
+		{
+			src_rotate *= -1;
+			dst_rotate *= -1;
+		}
+		if (src_rotate > dst_rotate)
+			return (src_rotate);
+		else
+			return (dst_rotate);
+	}
+	else
+	{
+		if (src_rotate - dst_rotate > 0)
+			return (src_rotate - dst_rotate);
+		else
+			return (dst_rotate - src_rotate);
+	}
 }
